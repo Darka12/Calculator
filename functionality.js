b407cc4e8b92b1c296de5a35d5c1e4eb3e950f6f
Oliver Horwood <thereviloproject@gmail.com>
2021-09-11 17:36:32 +0100

    basic operator functions

--- functionality.js
+++ functionality.js
@@ -1,10 +1,28 @@
-let a = 2;
+let a = 6;
 let b = 3;
 
 
-function add(a, b) {
+function add() {
     let result = a + b;
-    return result;
+    console.log(result);
+}
+
+function subtract() {
+    let result = a - b;
+    console.log(result);
+}
+
+function multiply() {
+    let result = a * b;
+    console.log(result);
+}
+
+function division() {
+    let result = a / b;
+    console.log(result);
 }
 
 add()
+subtract()
+multiply()
+division()
