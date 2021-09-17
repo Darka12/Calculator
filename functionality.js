fde598e68b954a1399e28b4ea54710018e414589
Oliver Horwood <thereviloproject@gmail.com>
2021-09-17 18:00:00 +0100

    setup basic gui

--- functionality.js
+++ functionality.js
@@ -1,28 +1,4 @@
-let a;
-let b;
 
 
-function add() {
-    let result = a + b;
-    console.log(result);
-}
 
-function subtract() {
-    let result = a - b;
-    console.log(result);
-}
 
-function multiply() {
-    let result = a * b;
-    console.log(result);
-}
-
-function division() {
-    let result = a / b;
-    console.log(result);
-}
-
-add()
-subtract()
-multiply()
-division()
